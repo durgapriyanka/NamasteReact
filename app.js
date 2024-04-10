2b6d7fee8ba72146eb3b8a208a0a4e973ab820ef
DONTHAMSETTI Durga Priyanka <durga.priyanka.donthamsetti@hexagon.com>
2024-04-10 19:48:08 +0530

    Episode-1

---
+++ app.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", { class: "heading" }, "Hello World")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//reaact is placing heading in div tag which is root.
+const parent = React.createElement('div', { id: "parent" },
+                [React.createElement('div', { id: "child1" },
+                    [React.createElement('h1', {}, "Nested Heading 1"), React.createElement('h2', {}, "Nested Heading 2")]),
+                React.createElement('div', { id: "child1" },
+                    [React.createElement('h1', {}, "Nested Heading 1"), React.createElement('h2', {}, "Nested Heading 2")])])
+root.render(parent)
+console.log(parent)
